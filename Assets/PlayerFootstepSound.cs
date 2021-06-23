e4ae8f8ad4b5a06b3a7a7775ab481996b6588263
Appy <6334424123@student.chula.ac.th>
2021-06-24 00:17:50 +0700

    Animations and footstep
    - New sprite for ranged attack fully equipped with muzzle flash lighting
    - Added a footstep and the system for different terrains based on tag

---
+++ Assets/PlayerFootstepSound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepSound : MonoBehaviour
+{
+    [SerializeField] private LayerMask levelAndPlatformMask;
+    [SerializeField] private AudioSource _audio;
+    [SerializeField] private AudioClip[] footStepConcrete;
+    [SerializeField] private AudioClip footStepDirt;
+
+    public void PlayFootStep() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.parent.position, Vector2.down, Mathf.Infinity, levelAndPlatformMask);
+        if(hit.transform.gameObject.tag == "Level_Concrete") {
+            _audio.PlayOneShot(footStepConcrete[Random.Range(0, footStepConcrete.Length)], 1f);
+        }
+    }
+}
